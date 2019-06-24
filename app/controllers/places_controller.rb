037e11508712faf585c8d5c6675f4bb7fab8bd30
Ciara Anderson <thelaylinguist@gmail.com>
2019-06-24 23:26:14 +0000

    Pagination

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,6 +1,10 @@
 class PlacesController < ApplicationController
 
   def index
-    @places = Place.all
-  end
+   @places = Place.order("name").page(params[:page]).per_page(10)
+
+
+
+      end
+
 end
