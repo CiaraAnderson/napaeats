9e6342dccbc179fcf7ce4a48e714a4df2dad4918
Ciara Anderson <thelaylinguist@gmail.com>
2019-07-05 16:36:58 +0000

    Photo index/style

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,16 +1,11 @@
 class Place < ApplicationRecord
   belongs_to :user
   has_many :comments
-  
+  has_many :photos
+
   geocoded_by :address
   after_validation :geocode 
   validates :name, presence: true, length: { minimum: 4 }
   validates :address, presence: true
   validates :description, presence: true
-end
-
-
-class Photo < ApplicationRecord
-  belongs_to :place
-  has_many :photos
 end
